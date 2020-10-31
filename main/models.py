57cf1c0ae9b337d8dd89dab8437e5103e74b5407
Ilia <kul.2002.il@gmail.com>
2020-10-31 18:49:42 +0700

    Созданны модели.

---
+++ main/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+	def Meta(self):
+		pass
+
+
+class Question(models.Model):
+	title = models.CharField(max_length=255, verbose_name="Название", default='')
+	text = models.CharField(max_length=255, verbose_name="Текст", default='')
+
+	def __str__(self):
+		return self.title
+
+
+class Answer(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	text = models.CharField(max_length=255, verbose_name="Вариант", default='')
+
+	def __str__(self):
+		return self.text
+
+
+class Voice(models.Model):
+	answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.text
+
+
