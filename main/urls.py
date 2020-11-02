50f957f547ec0dddf7391be3ce4d01ce50ee5ee9
Ilia <kul.2002.il@gmail.com>
2020-11-02 21:58:19 +0700

    Нихрена из авторизации не работает.

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from .views import BBLoginView, BBLogoutView, BBPasswordChangeView, RegisterDoneView, RegisterUserView
 
 app_name = 'main'
 
 urlpatterns = [
 	path('', views.index),
+	path('accounts/login/', BBLoginView.as_view(), name="login"),
+	path('accounts/register/', RegisterUserView.as_view(), name='register'),
+	path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+	path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
+	path('accounts/logout/', BBLogoutView.as_view(), name="logout"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from .models import Question
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import UpdateView, CreateView
+from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+
+from .models import Question, User
+from .forms import RegisterUserForm, ChangeUserInfoForm
 
 
 def index(request):
@@ -8,4 +18,43 @@ def index(request):
 	return render(request, 'main/index.html', context=context)
 
 
+class BBLoginView(LoginView):
+	template_name = 'main/login.html'
 
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+	template_name = 'main/logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+	model = User
+	template_name = 'main/changeUserInfo.html'
+	form_class = ChangeUserInfoForm
+	success_url = reverse_lazy('myApp:profile')
+	success_message = 'Личные данные пользователя изменены'
+
+	def dispatch(self, request, *args, **kwargs):
+		self.user_id = request.user.pk
+		return super().dispatch(request, *args, **kwargs)
+
+	def get_object(self, queryset=None):
+		if not queryset:
+			queryset = self.get_queryset()
+		return get_object_or_404(queryset, pk=self.user_id)
+
+
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+	template_name = 'main/passwordChange.html'
+	success_url = reverse_lazy('myApp:profile')
+	success_message = 'Пароль пользователя изменен'
+
+
+class RegisterUserView(CreateView):
+	model = User
+	template_name = 'main/registerUser.html'
+	form_class = RegisterUserForm
+	success_url = reverse_lazy('myApp:login')
+
+
+class RegisterDoneView(TemplateView):
+	template_name = 'main/registerDone.html'
